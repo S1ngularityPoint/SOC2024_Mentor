33625f613343246c62b1779eda82dac5d6b91b9f
Shravan <shravan.s.raghavan@gmail.com>
2024-06-25 20:02:50 +0530

    .

--- Week5/linear/linear.cpp
+++ Week5/linear/linear.cpp
@@ -4,6 +4,14 @@ LinearRegression::LinearRegression(unsigned long D){
     d = D;
     weights = zeros(d,1);
     bias = 0;
+    epsilon = E;
+}
+
+void LinearRegression::l2loss(matrix X, matrix Y){
+    __size d1 = X.shape(), d2 = Y.shape();
+    if (d1.first != d2.first || d2.second != 1){
+
+    }
 }
 
 void LinearRegression::GD(){
--- Week5/linear/linear.h
+++ Week5/linear/linear.h
@@ -1,12 +1,28 @@
 #include "matrix/matrix.h"
+#define E 1e15
+
+typedef pair<unsigned long,unsigned long> __size;
 
 class LinearRegression{
-    public:
-        unsigned long d; // The dimensions of the data. The input is a 1xd vector
+    private:
+        unsigned long d; // The dimensions of the data. Each datapoint is a dx1 vector
         matrix weights; // The weights are a dx1 vector
-        double bias;
+        double bias; // The bias is a scalar
+        double epsilon; 
+    public:
+        /*
+            * Input is a matrix of dimensions d x n where each column of the matrix is a data point and there are n 
+            * datapoints each of dimensions d x 1.
+            * The true output values is another matrix of dimensions 1 x n 
+        */
         LinearRegression(unsigned long);
-        void loss();
+        pair<matrix,matrix> split(matrix, float); // To split the input dataset into ttrain and test
+        void l2loss(matrix,matrix); // Finds the loss given the weights vector, the input data set and the true output values
+        void l2lossDerivative();
+        void train();
+        void test();
+        matrix predict(matrix);
         void GD();
-
 };
+
+LinearRegression L;
